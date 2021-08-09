157be8e412e6a454e35e14e7edf07739f831eebc
Hoang Ngoc Linh <=>
2021-08-09 13:55:44 +0700

    search product by salon_cd

--- app/Customize/Controller/Admin/Product/ProductController.php
+++ app/Customize/Controller/Admin/Product/ProductController.php
@@ -28,7 +28,7 @@ use Eccube\Repository\Master\PageMaxRepository;
 use Eccube\Repository\Master\ProductStatusRepository;
 use Eccube\Repository\ProductClassRepository;
 use Eccube\Repository\ProductImageRepository;
-use Eccube\Repository\ProductRepository;
+use Customize\Repository\ProductRepository;
 use Eccube\Repository\TagRepository;
 use Eccube\Repository\TaxRuleRepository;
 use Eccube\Service\CsvExportService;
@@ -50,6 +50,222 @@ use Symfony\Component\Routing\RouterInterface;
 
 class ProductController extends \Eccube\Controller\Admin\Product\ProductController
 {
+    /**
+     * @var CsvExportService
+     */
+    protected $csvExportService;
+
+    /**
+     * @var ProductClassRepository
+     */
+    protected $productClassRepository;
+
+    /**
+     * @var ProductImageRepository
+     */
+    protected $productImageRepository;
+
+    /**
+     * @var TaxRuleRepository
+     */
+    protected $taxRuleRepository;
+
+    /**
+     * @var CategoryRepository
+     */
+    protected $categoryRepository;
+
+    /**
+     * @var ProductRepository
+     */
+    protected $productRepository;
+
+    /**
+     * @var BaseInfo
+     */
+    protected $BaseInfo;
+
+    /**
+     * @var PageMaxRepository
+     */
+    protected $pageMaxRepository;
+
+    /**
+     * @var ProductStatusRepository
+     */
+    protected $productStatusRepository;
+
+    /**
+     * @var TagRepository
+     */
+    protected $tagRepository;
+
+    /**
+     * ProductController constructor.
+     *
+     * @param CsvExportService $csvExportService
+     * @param ProductClassRepository $productClassRepository
+     * @param ProductImageRepository $productImageRepository
+     * @param TaxRuleRepository $taxRuleRepository
+     * @param CategoryRepository $categoryRepository
+     * @param ProductRepository $productRepository
+     * @param BaseInfoRepository $baseInfoRepository
+     * @param PageMaxRepository $pageMaxRepository
+     * @param ProductStatusRepository $productStatusRepository
+     * @param TagRepository $tagRepository
+     */
+    public function __construct(
+        CsvExportService $csvExportService,
+        ProductClassRepository $productClassRepository,
+        ProductImageRepository $productImageRepository,
+        TaxRuleRepository $taxRuleRepository,
+        CategoryRepository $categoryRepository,
+        ProductRepository $productRepository,
+        BaseInfoRepository $baseInfoRepository,
+        PageMaxRepository $pageMaxRepository,
+        ProductStatusRepository $productStatusRepository,
+        TagRepository $tagRepository
+    ) {
+        parent::__construct(
+            $csvExportService,
+            $productClassRepository,
+            $productImageRepository,
+            $taxRuleRepository,
+            $categoryRepository,
+            $productRepository,
+            $baseInfoRepository,
+            $pageMaxRepository,
+            $productStatusRepository,
+            $tagRepository
+        );
+    }
+
+    /**
+     * @Route("/%eccube_admin_route%/product", name="admin_product")
+     * @Route("/%eccube_admin_route%/product/page/{page_no}", requirements={"page_no" = "\d+"}, name="admin_product_page")
+     * @Template("@admin/Product/index.twig")
+     */
+    public function index(Request $request, $page_no = null, Paginator $paginator)
+    {
+        $builder = $this->formFactory
+            ->createBuilder(SearchProductType::class);
+
+        $event = new EventArgs(
+            [
+                'builder' => $builder,
+            ],
+            $request
+        );
+        $this->eventDispatcher->dispatch(EccubeEvents::ADMIN_PRODUCT_INDEX_INITIALIZE, $event);
+
+        $searchForm = $builder->getForm();
+
+        /**
+         * ページの表示件数は, 以下の順に優先される.
+         * - リクエストパラメータ
+         * - セッション
+         * - デフォルト値
+         * また, セッションに保存する際は mtb_page_maxと照合し, 一致した場合のみ保存する.
+         **/
+        $page_count = $this->session->get('eccube.admin.product.search.page_count',
+            $this->eccubeConfig->get('eccube_default_page_count'));
+
+        $page_count_param = (int) $request->get('page_count');
+        $pageMaxis = $this->pageMaxRepository->findAll();
+
+        if ($page_count_param) {
+            foreach ($pageMaxis as $pageMax) {
+                if ($page_count_param == $pageMax->getName()) {
+                    $page_count = $pageMax->getName();
+                    $this->session->set('eccube.admin.product.search.page_count', $page_count);
+                    break;
+                }
+            }
+        }
+
+        if ('POST' === $request->getMethod()) {
+            $searchForm->handleRequest($request);
+
+            if ($searchForm->isValid()) {
+                /**
+                 * 検索が実行された場合は, セッションに検索条件を保存する.
+                 * ページ番号は最初のページ番号に初期化する.
+                 */
+                $page_no = 1;
+                $searchData = $searchForm->getData();
+
+                // 検索条件, ページ番号をセッションに保持.
+                $this->session->set('eccube.admin.product.search', FormUtil::getViewData($searchForm));
+                $this->session->set('eccube.admin.product.search.page_no', $page_no);
+            } else {
+                // 検索エラーの際は, 詳細検索枠を開いてエラー表示する.
+                return [
+                    'searchForm' => $searchForm->createView(),
+                    'pagination' => [],
+                    'pageMaxis' => $pageMaxis,
+                    'page_no' => $page_no,
+                    'page_count' => $page_count,
+                    'has_errors' => true,
+                ];
+            }
+        } else {
+            if (null !== $page_no || $request->get('resume')) {
+                /*
+                 * ページ送りの場合または、他画面から戻ってきた場合は, セッションから検索条件を復旧する.
+                 */
+                if ($page_no) {
+                    // ページ送りで遷移した場合.
+                    $this->session->set('eccube.admin.product.search.page_no', (int) $page_no);
+                } else {
+                    // 他画面から遷移した場合.
+                    $page_no = $this->session->get('eccube.admin.product.search.page_no', 1);
+                }
+                $viewData = $this->session->get('eccube.admin.product.search', []);
+                $searchData = FormUtil::submitAndGetData($searchForm, $viewData);
+            } else {
+                /**
+                 * 初期表示の場合.
+                 */
+                $page_no = 1;
+                // submit default value
+                $viewData = FormUtil::getViewData($searchForm);
+                $searchData = FormUtil::submitAndGetData($searchForm, $viewData);
+
+                // セッション中の検索条件, ページ番号を初期化.
+                $this->session->set('eccube.admin.product.search', $viewData);
+                $this->session->set('eccube.admin.product.search.page_no', $page_no);
+            }
+        }
+
+        $searchData['salon_cd'] = $this->getMemberSalonCd();
+        $qb = $this->productRepository->getQueryBuilderBySearchDataForAdmin($searchData);
+
+        $event = new EventArgs(
+            [
+                'qb' => $qb,
+                'searchData' => $searchData,
+            ],
+            $request
+        );
+
+        $this->eventDispatcher->dispatch(EccubeEvents::ADMIN_PRODUCT_INDEX_SEARCH, $event);
+
+        $pagination = $paginator->paginate(
+            $qb,
+            $page_no,
+            $page_count
+        );
+
+        return [
+            'searchForm' => $searchForm->createView(),
+            'pagination' => $pagination,
+            'pageMaxis' => $pageMaxis,
+            'page_no' => $page_no,
+            'page_count' => $page_count,
+            'has_errors' => false,
+        ];
+    }
+
     /**
      * @Route("/%eccube_admin_route%/product/product/new", name="admin_product_product_new")
      * @Route("/%eccube_admin_route%/product/product/{id}/edit", requirements={"id" = "\d+"}, name="admin_product_product_edit")
